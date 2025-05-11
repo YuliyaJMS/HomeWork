71160b7161906ea7107f565fa324732e5011d06e
Neto test <uv9058390@gmail.com>
2025-05-11 11:25:42 +0300

    The revision has done hard1

--- src/Hard1.java
+++ src/Hard1.java
@@ -1,7 +1,7 @@
 public class Hard1 {
     public static void main(String[] args) {
         String line = "Hello world!";
-        String line2 = line.substring(6, 12);
+        String line2 = line.substring(6);
         System.out.println("Взятие подстроки c 6 по 11 символ - " + line2);
     }
 }
